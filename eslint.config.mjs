ce5b5573bbe6e918fe9ff5d4ba82fcd7bec1813a
no-regerts <darraler@gmail.com>
2026-09-01 22:11:04 +0300

    Update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ export default [{
       format: ["camelCase", "PascalCase"],
     }],
 
-    curly: "warn",
+    curly: "never",
     eqeqeq: "warn",
     "no-throw-literal": "warn",
     semi: "warn",
